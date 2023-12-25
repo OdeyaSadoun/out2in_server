8c3de77aecf6fa0af518d3b10e529886b1f31123
shira-aviram <s214124307@gmail.com>
2023-12-25 12:42:57 +0200

    add 5 first function in classes

---
+++ api/validations/classes.validation.js
@@ -0,0 +1,19 @@
+const Joi = require("joi");
+
+exports.classValidate = (_reqBody) => {
+  let classValidate = Joi.object({
+    school_id: Joi.string().required(), 
+    name: Joi.string().min(2).max(50).required(),
+    places: Joi.array().items(Joi.object({
+      name: Joi.string().required(),
+      grade: Joi.string().required(),
+  
+    })),
+    active: Joi.boolean().required(),
+    date_created: Joi.date().default(new Date(), "current date"),
+
+
+  });
+
+  return classValidate.validate(_reqBody);
+};
