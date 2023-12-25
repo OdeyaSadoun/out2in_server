a9002d73be8d665da0b68547198ab73444309789
אודיה סעדון <osadoun@g.jct.ac.il>
2023-12-25 11:57:01 +0200

    add auth by role

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,38 +1,42 @@
 const jwt = require("jsonwebtoken");
-const {config} = require("../config/secret");
+const { config } = require("../config/secret");
 
-exports.auth = (req,res,next) => {
-  let token = req.header("x-api-key");
-  if(!token){
-    return res.status(401).json({msg:"You need to send token to this endpoint url"})
-  }
-  try{
-    let decodeToken = jwt.verify(token,config.tokenSecret);
-    req.tokenData = decodeToken;
-    next();
-  }
-  catch(err){
-    console.log(err);
-    return res.status(401).json({msg:"Token invalid or expired, log in again or you hacker!"})
-  }
-}
-
-exports.authAdmin = (req,res,next) => {
-  let token = req.header("x-api-key");
-  if(!token){
-    return res.status(401).json({msg:"You need to send token to this endpoint url"})
-  }
-  try{
-    let decodeToken = jwt.verify(token,config.tokenSecret);
-    if(decodeToken.role != "admin"){
-      return res.status(401).json({msg:"Token invalid or expired, code: 6A"})
+exports.authRole = (roles) => {
+  return (req, res, next) => {
+    let token = req.header("x-api-key");
+    try {
+      let decodeToken = jwt.verify(token, config.tokenSecret);
+      if (roles.indexOf(decodeToken.role) == -1) {
+        
+        return res
+          .status(401)
+          .json({ msg: "Token invalid or expired, code: 6A" });
+      }
+      next();
+    } catch (err) {
+      console.log(err);
+      return res
+        .status(401)
+        .json({ msg: "Token invalid or expired, log in again or you hacker!" });
     }
-   
+  };
+};
+
+exports.auth = (req, res, next) => {
+  let token = req.header("x-api-key");
+  if (!token) {
+    return res
+      .status(401)
+      .json({ msg: "You need to send token to this endpoint url" });
+  }
+  try {
+    let decodeToken = jwt.verify(token, config.tokenSecret);
     req.tokenData = decodeToken;
     next();
-  }
-  catch(err){
+  } catch (err) {
     console.log(err);
-    return res.status(401).json({msg:"Token invalid or expired, log in again or you hacker!"})
+    return res
+      .status(401)
+      .json({ msg: "Token invalid or expired, log in again or you hacker!" });
   }
-}
+};
