6489f0e794ecbb791a074f7a8087f7b271976710
shira-aviram <s214124307@gmail.com>
2023-12-26 10:59:50 +0200

    classes routers b - finish classes Fixes #28

--- api/routes/classes.js
+++ api/routes/classes.js
@@ -5,21 +5,21 @@ const { classCtrl } = require("../controllers/classes.controller");
 const { auth, authRole } = require("../middleware/auth");
 
 
-router.get("/classes/:id", auth, authRole(["admin","principal", "teacher"]), classCtrl.getClassById);
-router.get("/classes/students", auth, authRole(["admin","principal", "teacher","student"]), classCtrl.getAllStudentsInClass);
-router.get("/classes",auth, authRole(["admin","principal", "teacher"]), classCtrl.getAllClasses);
-router.get("/classes/places",auth, authRole(["admin","principal", "teacher"]), classCtrl.getAllPlaces);
+router.get("/:id", auth, authRole(["admin","principal", "teacher"]), classCtrl.getClassById);
+router.get("/students", auth, authRole(["admin","principal", "teacher","student"]), classCtrl.getAllStudentsInClass);
+router.get("/",classCtrl.getAllClasses);
+router.get("/places",auth, authRole(["admin","principal", "teacher"]), classCtrl.getAllPlaces);
 
 // router.get("/classes/distribution", classesCtrl.getClassAttendanceDistribution);
 // router.get("/classes/distribution/:id", classesCtrl.getAttendanceDistributionForStudent);
 // router.post("/classes/attendance", classesCtrl.fillClassAttendance);
-router.post("/classes/places", auth, authRole(["admin","principal", "teacher"]),classCtrl.addPlacesToClass);
-router.post("/classes",auth, authRole(["admin","principal", "teacher"]), classCtrl.addClass);
+router.post("/places", auth, authRole(["admin","principal", "teacher"]),classCtrl.addPlacesToClass);
+router.post("/",auth, authRole(["admin","principal", "teacher"]), classCtrl.addClass);
 
 // router.put("/classes/attendance/:id", classesCtrl.updateAttendanceForStudent);
-router.put("/classes/addTeacherClass",auth, authRole(["admin","principal", "teacher"]), classCtrl.addClassToTeacher);
-router.put("/classes/:id",auth, authRole(["admin","principal", "teacher"]), classCtrl.updateClass);
+router.put("/addTeacherClass",auth, authRole(["admin","principal", "teacher"]), classCtrl.addClassToTeacher);
+router.put("/:id",auth, authRole(["admin","principal", "teacher"]), classCtrl.updateClass);
 
-router.delete("/classes/:id",auth, authRole(["admin","principal", "teacher"]), classCtrl.deleteClass);
+router.delete("/:id",auth, authRole(["admin","principal", "teacher"]), classCtrl.deleteClass);
 
 module.exports = router;
