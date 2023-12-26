37f427066d7616e2500a40e9d991480838611a81
shira-aviram <s214124307@gmail.com>
2023-12-26 11:44:47 +0200

    finish classes and message

---
+++ api/routes/messages.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const router = express.Router();
+const { authRole, auth } = require("../middleware/auth");
+const { messagesCtrl } = require("../controllers/messages.controllers");
+
+router.get("/:id",auth,authRole(["admin"]),messagesCtrl.getMessagesByUserId);
+router.post("/:id",auth,authRole(["admin"]),messagesCtrl.sendMessageToTeacher);
+router.post("/",auth,authRole(["admin","student","teacher","principal"]),messagesCtrl.sendMessagesToAll);
+router.put("/:id",auth,authRole(["admin"]),messagesCtrl.deleteMessage);
+
+
+module.exports = router;
