524b988ec7e453d9fad0e7f5c52fb39a3658b2f3
rivkish <rivki3232@gmail.com>
2023-12-26 09:28:45 +0200

    not work correct

--- api/controllers/teachers.controller.js
+++ api/controllers/teachers.controller.js
@@ -1,23 +1,38 @@
 const { TeacherModel } = require("../models/teachers.model");
 const { userlCtrl } = require("../controllers/users.controller");
 const { UserModel } = require("../models/users.model");
+const { SchoolsModel } = require("../models/schools.model");
 
 
 exports.teacherlCtrl = {
-    getTeacherInfo:async(req, res)=>{
-        
-        // res.json(req.userInfo)
-        try {
-            let teacherInfo = await TeacherModel.findOne(
-              { user_id: req.tokenData._id }
-            );
-            res.json({"user":req.userInfo,"teacher info":teacherInfo});
-          } catch (err) {
-            console.log(err);
-            res.status(500).json({ msg: "err", err });
-          }
-       
-   
+  getTeacherInfo: async (req, res) => {
+
+    // res.json(req.userInfo)
+    try {
+      let teacherInfo = await TeacherModel.findOne(
+        { user_id: req.tokenData._id }
+      );
+      res.json({ "user": req.userInfo, "teacher info": teacherInfo });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ msg: "err", err });
     }
-  };
-  
+
+
+  },
+  getAllTeachers: async (req, res) => {
+    try {
+      console.log(req.tokenData._id)
+      let school = await SchoolsModel.findOne({ principal_id: req.tokenData._id });
+      console.log("school", school)
+      let data = await TeacherModel.find({});
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ msg: "err", err });
+    }
+  },
+
+
+};
+
