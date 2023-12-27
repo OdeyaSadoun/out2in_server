a62847f5df5c346e5e7716dffd4211fd6a80c6c0
rivkish <rivki3232@gmail.com>
2023-12-27 10:20:10 +0200

    subject not work

--- api/routes/subjects.js
+++ api/routes/subjects.js
@@ -4,5 +4,6 @@ const {subjectsCtrl } = require('../controllers/subjects.controller');
 const { auth, authRole } = require('../middleware/auth');
 
 router.get('/',auth,authRole(["admin"]),subjectsCtrl.getAllSubjects);
+router.get('/getSubjectByStudent/',auth,authRole(["teacher"]),subjectsCtrl.getSubjectsByStudentId);
 
 module.exports=router;
