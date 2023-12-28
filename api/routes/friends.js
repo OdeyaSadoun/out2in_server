4d79618566c3e98c9447c128486591a26fda4451
אודיה סעדון <osadoun@g.jct.ac.il>
2023-12-28 20:55:16 +0200

    change the router request

--- api/routes/friends.js
+++ api/routes/friends.js
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.post("/questionnaire", auth, authRole("student"), friendCtrl.addNewQuestionnaireAnswer);
 router.get("/", auth, authRole("teacher"), friendCtrl.getFriendsList); //to do check that this teacher connect to this class
-router.post("/calcSocialIndexStudents", auth, authRole("teacher"), friendCtrl.calcSocialIndexStudentsByQuestionnaire); //to do check that this teacher connect to this class
+router.post("/calcSocialIndexStudentsByQuestionnaire", auth, authRole("teacher"), friendCtrl.calcSocialIndexStudentsByQuestionnaire); //to do check that this teacher connect to this class
 
 module.exports = router;
