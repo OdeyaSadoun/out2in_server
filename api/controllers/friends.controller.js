06976bf1bb8123a3650bbfa62995c6d3594493b7
אודיה סעדון <osadoun@g.jct.ac.il>
2023-12-31 23:43:14 +0200

    fix in visability of friends controller

--- api/controllers/friends.controller.js
+++ api/controllers/friends.controller.js
@@ -5,22 +5,40 @@ const { TeacherModel } = require("../models/teachers.model");
 const { StudentModel } = require("../models/students.model");
 
 exports.friendCtrl = {
+  getFriendsList: async (req, res) => {
+    try {
+      let student = await StudentModel.find({});
+      let teacher = await TeacherModel.findOne({ user_id: req.tokenData._id });
+      let friendsJson = student.filter((stud) => {
+        return teacher.classes_list.includes(stud.class_id);
+      });
+      let friendsJsonID = friendsJson.map((f) => String(f.user_id));
+      let data = await FriendModel.find({});
+      let friendsByTeacher = data.filter((fr) => {
+        return friendsJsonID.includes(String(fr.student));
+      });
+      res.json(friendsByTeacher);
+    } catch (error) {
+      console.log(err);
+      res.status(500).json({ msg: "err", err });
+    }
+  },
+
   addNewQuestionnaireAnswer: async (req, res) => {
     try {
-
-      const friends_list = req.body.friends;
+      const friendsList = req.body.friends;
       const student = req.tokenData._id;
       let newAnswer;
 
-      console.log(friends_list);
+      console.log(friendsList);
 
       if (!student) {
         return res.status(404).json({ error: "Student not found" });
       }
-      if (friends_list.length > 0) {
+      if (friendsList.length > 0) {
         newAnswer = new FriendModel({
-          "student": student,
-          "friends": friends_list,
+          student: student,
+          friends: friendsList,
         });
       }
 
@@ -32,46 +50,10 @@ exports.friendCtrl = {
       res.status(500).json({ error: "Internal Server Error" });
     }
   },
-  getFriendsList: async (req, res) => {
-    try {
-      let data1 = await StudentModel.find({});
-      let teacher = await TeacherModel.findOne({ user_id: req.tokenData._id });
-      let friendsJson = data1.filter((stud) => {
-               return teacher.classes_list.includes(stud.class_id);
-            });
-      let friendsJsonID=friendsJson.map(f=> String(f.user_id))
-      let data = await FriendModel.find({});
-      let friendsByTeacher=data.filter((fr)=>{
-        return friendsJsonID.includes(String(fr.student))
-      })
-      res.json(friendsByTeacher);
-    } catch (error) {
-      console.log(err);
-      res.status(500).json({ msg: "err", err });
-    }
-  },
-  // getAllStudentsTeacher: async (req, res) => {
-  //   try {
-  //     let data = await StudentModel.find({}).populate("user_id", {
-  //       password: 0,
-  //     });
-  //     // console.log(req.tokenData._id);
-  //     let teacher = await TeacherModel.findOne({ user_id: req.tokenData._id });
 
-  //     // console.log(teacher);
-  //     let studentJson = data.filter((stud) => {
-  //       return teacher.classes_list.includes(stud.class_id);
-  //     });
-  //     // console.log(studentJson);
-  //     res.json(studentJson);
-  //   } catch (err) {
-  //     console.log(err);
-  //     res.status(500).json({ msg: "err", err });
-  //   }
-  // },
   calcSocialIndexStudentsByQuestionnaire: async (req, res) => {
     try {
-      const  friends_list  = req.body;
+      const friends_list = req.body;
       try {
         const response = await axios.post(
           "http://127.0.0.1:5000/calculate_social_index",
