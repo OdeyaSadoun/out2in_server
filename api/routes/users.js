0b381132aeb8d57e157e8410eb6edb0e1ff2d145
אודיה סעדון <osadoun@g.jct.ac.il>
2024-01-03 01:11:27 +0200

    fix

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,26 +3,36 @@ const { auth, authRole } = require("../middleware/auth");
 const { userlCtrl } = require("../controllers/users.controller");
 const { authCtrl } = require("../controllers/auth.controller");
 const router = express.Router();
-
+router.get("/usersList", auth, authRole("admin"), userlCtrl.getAllUsers);
+router.get("/getCurrentUser", auth, userlCtrl.getCurrentUser);
+router.get("/getUser/:id", auth, userlCtrl.getUserById);
 router.post("/register/principal", authCtrl.registerPrincipal);
-
-router.post("/register/teacher",auth,authRole("principal"), authCtrl.registerTeacher); 
-
-router.post("/register/student",auth,authRole("teacher"), authCtrl.registerStudent);
-
+router.post(
+  "/register/teacher",
+  auth,
+  authRole("principal"),
+  authCtrl.registerTeacher
+);
+router.post(
+  "/register/student",
+  auth,
+  authRole("teacher"),
+  authCtrl.registerStudent
+);
 router.post("/login", authCtrl.login);
 
 router.post("/logout", authCtrl.logout);
 
 // router.get("/myInfo", auth, userlCtrl.getUserInfo);
 
-router.get("/usersList",auth,authRole("admin"), userlCtrl.getAllUsers);
-router.get("/getCurrentUser",auth, userlCtrl.getCurrentUser);
-router.get("/getUser/:id",auth, userlCtrl.getUserById);
-
 router.put("/edit/:idEdit", auth, userlCtrl.editUser);
 
-router.patch("/delete/", auth,authRole(["admin","teacher","principal"]), userlCtrl.deleteUser);
+router.patch(
+  "/delete/",
+  auth,
+  authRole(["admin", "teacher", "principal"]),
+  userlCtrl.deleteUser
+);
 
 router.patch("/changePassword", auth, authCtrl.changePassword);
 
