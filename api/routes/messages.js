4e30f5bf599c59720dbd7962dae161ad501339f3
shoham bitton <shoham8988@gmail.com>
2024-01-02 15:04:30 +0200

    כ

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const { authRole, auth } = require("../middleware/auth");
 const { messagesCtrl } = require("../controllers/messages.controllers");
 
-router.get("/:id",auth,authRole(["admin"]),messagesCtrl.getMessagesByUserId);
+router.get("/:teacherId",auth,authRole(["admin","teacher"]),messagesCtrl.getMessagesByUserId);
 
-router.post("/:id",auth,authRole(["admin","student"]),messagesCtrl.sendMessageToTeacher);
+router.post("/:studentId",auth,authRole(["admin","student"]),messagesCtrl.sendMessageToTeacher);
 
 router.post("/",auth,authRole(["admin","student","teacher","principal"]),messagesCtrl.sendMessagesToAll);
 
