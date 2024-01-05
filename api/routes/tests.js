26a63f1627322ceb2264da88738a8aadcacf425b
אודיה סעדון <osadoun@g.jct.ac.il>
2024-01-05 13:41:51 +0200

    little fix- not online delete

--- api/routes/tests.js
+++ api/routes/tests.js
@@ -4,7 +4,7 @@ const { auth, authRole } = require('../middleware/auth');
 const { testsCtrl } = require('../controllers/tests.controller');
 
 // router.get ('/:id', auth, authRole(["teacher", "admin"]),testsCtrl.getTestById)
-router.get ('/:testid', auth, authRole(["teacher", "admin"]),testsCtrl.getTestById)
+router.get ('/:testId', auth, authRole(["teacher", "admin"]),testsCtrl.getTestById)
 router.post ('/addGrades',auth,authRole(["teacher", "admin"]),testsCtrl.addGrades);//grade send in body
 router.patch ('/deleteTest/:testId',auth,authRole(["teacher", "admin"]),testsCtrl.deleteTest);//grade send in body
 // router.post ('/addTest',auth,authRole(["teacher"]),testsCtrl.addTest);//grade send in body
