847a0223432c6165160bae1930e56f67f920b1da
shoham bitton <shoham8988@gmail.com>
2024-01-07 13:20:57 +0200

    attendence students Fixes #46

--- api/controllers/tests.controller.js
+++ api/controllers/tests.controller.js
@@ -5,48 +5,73 @@ const { ClassModel } = require("../models/classes.model");
 const { UserModel } = require("../models/users.model");
 const { TestModel } = require("../models/tests.model");
 
+const getTestsBalanceByStudentId1 = async (studentId,class_id) => {
+  let gradesSum1 = 0;
+  let gradesSum2 = 0;
+  let gradesSum3 = 0;
+
+  try {
+    // const student = await StudentModel.findOne({ _id: studentId }).populate("user_id", { password: 0 });
+    const studentClass = await ClassModel.findOne({ _id: class_id }).populate("subjects_list");
+    const subjectsList = studentClass.subjects_list;
+
+    for (const subject of subjectsList) {
+      const lastTestsIds = subject.tests_list.slice(-3);
+
+      for (const testId of lastTestsIds) {
+        const test = await TestModel.findOne({ _id: testId, active: true });
+
+        if (test) {
+          const studentGrade = test.grades_list.find((grade) => String(grade.student_id) === String(studentId));
+
+          if (studentGrade) {
+            switch (lastTestsIds.indexOf(testId)) {
+              case 0:
+                gradesSum1 += Number(studentGrade.grade);
+                break;
+              case 1:
+                gradesSum2 += Number(studentGrade.grade);
+                break;
+              case 2:
+                gradesSum3 += Number(studentGrade.grade);
+                break;
+            }
+          }
+        }
+      }
+    }
+
+    const subjectsCount = subjectsList.length > 0 ? subjectsList.length : 1; // To avoid division by zero
+    const avgGrades1 = gradesSum1 / subjectsCount;
+    const avgGrades2 = gradesSum2 / subjectsCount;
+    const avgGrades3 = gradesSum3 / subjectsCount;
+    if (avgGrades1 > avgGrades2 > avgGrades3)
+      return true;
+    else
+      return false
+    // return { gradesAvg1: avgGrades1, gradesAvg2: avgGrades2, gradesAvg3: avgGrades3 };
+  } catch (err) {
+    console.log({ msg: "err", err });
+    return { error: "Internal server error" };
+  }
+}
+
 exports.testsCtrl = {
-  GetTestsBalanceByStudentId: async (req, res) => {
-    let gradesSum = 0;
-    const studentId = req.params.studentId;
-    let student_class;
-    try {
-      const student = await StudentModel.findOne({
-        _id: studentId,
-      }).populate("user_id", { password: 0 });
+  getTestsBalanceByStudentId: async (req, res) => {
 
-      student_class = await ClassModel.findOne({ _id: student.class_id })
-        .populate("subjects_list");
-
-      const subjects_list = student_class.subjects_list;
-      await subjects_list.map(async (subject, i) => {
-
-        let last_test_id = subject.tests_list.slice(-3);
-        // console.log(last_test_id);
-        
-        const last_test = await TestModel.findOne({ _id: last_test_id })
-        console.log(last_test);
-        const student_grade = last_test?.grades_list.filter((grade) => {
-          if (String(grade.student_id) === String(studentId))
-            return String(grade.student_id) === String(studentId)
-        });
-        if (student_grade) {
-          gradesSum += Number(student_grade[0].grade);
-        }
-        if (i == subjects_list.length - 1) {
-          res.json({ "gradesAvg": gradesSum / (subjects_list.length - 1) })
-        }
-      })
+    const classId = req.params.classId;
+    const classStudents = await StudentModel.find({ class_id: classId });
+    const class_balance_arr = [];
+    for (const student of classStudents) {
+      let avg = await getTestsBalanceByStudentId1(student._id,classId)
+      class_balance_arr.push({ srudent_id: student._id, avg: avg });
     }
-    catch (err) {
-      console.log({ msg: "err", err });
-    }
+    res.json(class_balance_arr);
   },
-
   addTestToSubject: async (req, res) => {
     const subId = req.params.subId;
     const testId = req.body.test_id;
- 
+
     let subject = await SubjectsModel.findOne({ _id: subId });
 
     let data = await SubjectsModel.updateOne(
@@ -67,3 +92,9 @@ exports.testsCtrl = {
     }
   },
 };
+
+
+
+
+
+
