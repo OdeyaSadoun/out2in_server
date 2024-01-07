3a245e1ccdec6a84e05ad61a89f80ed76da62786
shoham bitton <shoham8988@gmail.com>
2024-01-07 14:18:47 +0200

    valid ok

--- api/validations/classes.validation.js
+++ api/validations/classes.validation.js
@@ -2,12 +2,11 @@ const Joi = require("joi");
 
 exports.classValidate = (_reqBody) => {
   let classValidate = Joi.object({
-    school_id: Joi.string().required(), 
+    school_id: Joi.required(),
     name: Joi.string().min(2).max(50).required(),
     places: Joi.array().items(Joi.object({
       name: Joi.string().required(),
       grade: Joi.string().required(),
-  
     })),
     active: Joi.boolean().required(),
     date_created: Joi.date().default(new Date(), "current date"),
